7f3c612f1f846d1169190c4005d3dcbe90c93a1d
Jheymisson Albuquerque <jheymis.albuquerque@hotmail.com>
2024-01-14 22:44:31 -0300

    criado requisicao de buscar todos os cursos e criar curso

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from fastapi import HTTPException
+from fastapi import status
+
+from models import Curso
 
 app = FastAPI()
 
@@ -20,6 +24,29 @@ cursos = {
     }
 }
 
+@app.get('/cursos')
+async def get_cursos():
+    return cursos
+
+@app.get('/cursos/{curso_id}')
+async def get_curso(curso_id: int):
+    try:
+        curso = cursos[curso_id]
+        return curso
+    except KeyError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Cursos nao encontrado')
+
+
+@app.post('/cursos')
+async def post_curso(curso: Curso):
+    if curso.id is None:
+        curso.id = max(cursos.keys(), default=0) + 1
+
+    if curso.id not in cursos:
+        cursos[curso.id] = curso.model_dump()
+        return cursos[curso.id]
+
+
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run('main:app', host='0.0.0.0', port=8000)
+    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
